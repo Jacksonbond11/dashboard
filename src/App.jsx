13869b2140b5861b65c0cf32b442fbf0d93d5c67
Jackson Bond <jacksonbond11@yahoo.com>
2024-11-16 22:28:31 -0600

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Card from "./components/card.jsx"
+
+function App() {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <div style={{ display: "flex", justifyContent: "space-between", gap: "20px" }}>
+      <Card img="/portainer.jpg" title="Portainer" link="https://192.168.50.26:32777"/>
+      <Card img="/mealie.png" title="Mealie" link="https://192.168.50.26:32777"/>
+      <Card img="/pihole.jpg" title="Pi-hole" link="https://192.168.50.26:32777"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card = ({ img, title, link }) => {
+  return (
+    <a href={link} style={{ textDecoration: "none" }}>
+      <div
+        style={{
+     
+          padding: "10px",
+          transition: "background-color 0.3s",
+        }}
+        onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = "#424242")}
+        onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = "transparent")}
+      >
+        <img src={img} alt={title} style={{ width: "100px" }} />
+        <h3>{title}</h3>
+      </div>
+    </a>
+  );
+};
+
+export default Card;
